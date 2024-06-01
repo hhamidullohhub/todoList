787fc489673da2ad7827f5ee9e0d79be0acf7df3
MRX <131702289+hhamidullohhub@users.noreply.github.com>
2024-06-01 10:54:18 +0500

    creat array and object

---
+++ ]toDo.js
@@ -0,0 +1,49 @@
+const input = document.getElementById('input')
+const button = document.getElementById('button')
+const ul = document.getElementById('ul')
+let peoples = []
+
+
+function renderList() {
+    ul.innerHTML = null
+
+    peoples.forEach((el) => {
+        const newli = document.createElement('li')
+        const deleteButton = document.createElement('button')
+
+        newli.textContent = el.name
+        newli.classList.add('li')
+        deleteButton.textContent = 'delete'
+
+        deleteButton.setAttribute('onclick', `deleteUser(${el.id})`)
+
+        newli.append(deleteButton)
+        ul.prepend(newli)
+
+    })
+    input.value = ''
+}
+
+button.addEventListener('click', () => {
+    const inputValue = input.value
+
+    if (inputValue == '') {
+        alert('qiymat kiriting')
+    } else {
+        const person = {
+            id: Date.now(),
+            name: inputValue
+        }
+
+        peoples.push(person)
+        console.log(peoples);
+        renderList()
+    }
+})
+
+function deleteUser(id) {
+    console.log(id)
+    const newUser = person.filter(person => person.id !== id)
+    person= [...newUser]
+    renderList()
+}
